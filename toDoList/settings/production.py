2cd32704a23b893e221dbc183bb15b1c548dbd13
Conar <galiev25@list.ru>
2020-09-26 20:01:41 +0300

    Production settings fixed

--- toDoList/settings/production.py
+++ toDoList/settings/production.py
@@ -7,4 +7,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
