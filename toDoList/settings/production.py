d114095d8edc9194a54605c9fb1ba296143e8e1d
Conar <galiev25@list.ru>
2020-09-26 20:07:37 +0300

    ALLOWED_HOSTS fixed in production settings

--- toDoList/settings/production.py
+++ toDoList/settings/production.py
@@ -5,7 +5,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ilshat-to-do.herokuapp.com']
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
